83a3ce7cc30e9675fbcfa5069d5de1fe91f0b31a
oleksiibohusevych-118 <165788074+oleksiibohusevych-118@users.noreply.github.com>
2025-03-13 22:58:15 +0200

    Add project files.

---
+++ TreeDH/GroupDiffieHellman.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+namespace TreeDH
+{
+    class GroupDiffieHellman
+    {
+        private static readonly BigInteger P = 23;
+        private static readonly BigInteger G = 5;
+
+        public static BigInteger ModExp(BigInteger baseValue, BigInteger exponent, BigInteger modulus) => BigInteger.ModPow(baseValue, exponent, modulus);
+
+        public static BigInteger GeneratePrivateKey() => new Random().Next(1, (int)P - 1);
+
+        public static BigInteger ComputePublicKey(BigInteger privateKey) => ModExp(G, privateKey, P);
+
+        public static BigInteger ComputeSharedKey(BigInteger receivedKey, BigInteger privateKey) => ModExp(receivedKey, privateKey, P);
+
+        public static BigInteger ComputeTreeDH(List<BigInteger> privateKeys)
+        {
+            var currentLevel = new List<BigInteger>();
+
+            foreach (var privateKey in privateKeys)
+            {
+                currentLevel.Add(ComputePublicKey(privateKey));
+            }
+
+            while (currentLevel.Count > 1)
+            {
+                var nextLevel = new List<BigInteger>();
+
+                for (int i = 0; i < currentLevel.Count; i += 2)
+                {
+                    if (i + 1 < currentLevel.Count)
+                    {
+                        BigInteger sharedKey = ComputeSharedKey(currentLevel[i + 1], privateKeys[i]);
+                        nextLevel.Add(sharedKey);
+                    }
+                    else
+                    {
+                        nextLevel.Add(currentLevel[i]);
+                    }
+                }
+
+                currentLevel = nextLevel;
+            }
+
+            return currentLevel[0];
+        }
+    }
+}
---
+++ TreeDH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using TreeDH;
+
+Console.WriteLine("Hello, World!");
+
+int numberOfUsers = 5;
+List<BigInteger> privateKeys = new List<BigInteger>();
+
+for (int i = 0; i < numberOfUsers; i++)
+{
+    privateKeys.Add(GroupDiffieHellman.GeneratePrivateKey());
+}
+
+BigInteger finalSharedKey = GroupDiffieHellman.ComputeTreeDH(privateKeys);
+
+Console.WriteLine("Private keys:");
+for (int i = 0; i < privateKeys.Count; i++)
+{
+    Console.WriteLine($"User {i + 1}: {privateKeys[i]}");
+}
+
+Console.WriteLine($"\nFinal shared key (g^(x1*x2*...*xn) mod P): {finalSharedKey}");
